cc78f19bb8212171361202785e9581c790e1126e
koshike-saikumar <koshikesai25@gmail.com>
2025-06-23 15:02:30 +0530

    remove some unwantend import

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,4 +1,3 @@
-import React from "react";
 import "./Footer.css"; 
 
 const Footer = () => {  
@@ -15,12 +14,12 @@ const Footer = () => {
         <div className="footer-links">
           <h3>Quick Links</h3>
           <ul>
-            <li><a href="#hero">Home</a></li>
-            <li><a href="#about">About</a></li>
+            <li><a href="/hero">Home</a></li>
+            <li><a href="/about">About</a></li>
             {/* <li><Link to="/jobseeker/jobs">Browse Jobs</Link></li> */}
                         <li><a href="#">Browse Jobs</a></li>
 
-            <li><a href="#company">Companies</a></li>
+            <li><a href="/company">Companies</a></li>
           </ul>
         </div>
 
@@ -28,9 +27,9 @@ const Footer = () => {
         <div className="footer-social">
           <h3>Follow Us</h3>
           <div className="social-icons">
-            <a href="#" target="_blank" rel="noopener noreferrer">🔵 Github</a>
-            <a href="#" target="_blank" rel="noopener noreferrer">🔵 Twitter</a>
-            <a href="#" target="_blank" rel="noopener noreferrer">🔵 LinkedIn</a>
+            <a href="/login" target="_blank" rel="noopener noreferrer">🔵 Github</a>
+            <a href="/login" target="_blank" rel="noopener noreferrer">🔵 Twitter</a>
+            <a href="/login" target="_blank" rel="noopener noreferrer">🔵 LinkedIn</a>
           </div>
         </div>
       </div>
